d2f4f0a36249f3ec9c3f917bc689fc3f0d09e206
singaseong96 <singseong@kakao.com>
2026-05-19 13:30:13 +0900

    에러 코드 수정

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -9,9 +9,9 @@ export const ROUTES = {
   ANALYSIS_CHAT: '/analysis/chat',
   ANALYSIS_REPORT: '/analysis/report',
   RESUME: '/resume',
+  APPLICATIONS: '/applications',
   INTERVIEW: '/interview',
   INTERVIEW_DETAIL: '/interview/:id',
   MYPAGE: '/mypage',
-  APPLICATIONS: '/applications',
   COMPANY: '/company',
 } as const;
